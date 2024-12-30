0f7a8603e2342afd6c2ed50ed5e2245cc6c2249d
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2024-12-30 15:02:54 -0800

    created boxes, need to keep it from overflowing out of screen

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 function divCreator(width, height){
     const mainContainer = document.getElementById("mainDiv");
     mainContainer.style.display = "flex";
+    mainContainer.width = "100%";
+    mainContainer.height = "100%";
+    mainContainer.style.justifyContent = "center";
+    mainContainer.style.alignItems = "center";
+    mainContainer.style.flexWrap = "wrap";
     for (let i = 0; i < width; i++)
     {
         const widthDiv = document.createElement("div");
-        widthDiv.style.width ="36px";
-        widthDiv.style.height = "36px";
+        widthDiv.style.display = "flex";
+        widthDiv.style.width ="25%";
         widthDiv.textContent = "um";
+        widthDiv.style.aspectRatio = "1";
         mainContainer.appendChild(widthDiv);
         for (let j = 0; j < height; j++)
         {
             const heightDiv = document.createElement("div");
-            heightDiv.style.width = "36px";
-            heightDiv.style.height = "36px";
+            heightDiv.style.display = "flex";
+            heightDiv.style.width = "25%";
             heightDiv.textContent = "hi";
             mainContainer.appendChild(heightDiv)
             break;
