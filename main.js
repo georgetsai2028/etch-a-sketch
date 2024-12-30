5cec820823bd770bd55f6a295d6853c9ac342511
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2024-12-30 15:34:16 -0800

    made grid fit on screen, need to make grid back into squares

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function divCreator(width, height){
     const mainContainer = document.getElementById("mainDiv");
-    mainContainer.style.display = "flex";
-    mainContainer.width = "100%";
-    mainContainer.height = "100%";
+    mainContainer.style.display = "grid";
+    mainContainer.style.width = "100vw";
+    mainContainer.style.height = "100vh";
     mainContainer.style.justifyContent = "center";
     mainContainer.style.alignItems = "center";
     mainContainer.style.flexWrap = "wrap";
@@ -11,19 +11,23 @@ function divCreator(width, height){
         const widthDiv = document.createElement("div");
         widthDiv.style.display = "flex";
         widthDiv.style.width ="25%";
-        widthDiv.textContent = "um";
-        widthDiv.style.aspectRatio = "1";
         mainContainer.appendChild(widthDiv);
+        widthDiv.addEventListener("mouseenter", () => {
+            widthDiv.style.backgroundColor = "black";
+        })
         for (let j = 0; j < height; j++)
         {
             const heightDiv = document.createElement("div");
             heightDiv.style.display = "flex";
             heightDiv.style.width = "25%";
-            heightDiv.textContent = "hi";
             mainContainer.appendChild(heightDiv)
+            heightDiv.addEventListener("mouseenter", () => {
+                heightDiv.style.backgroundColor = "black";
+            })
             break;
             
         }
     }
 }
 divCreator(16,16);
+
